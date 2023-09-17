e1154828cfc9837f47473cc2d01b659c7d1393d9
khanhtt <khanhtrantung190894@gmail.com>
2023-09-17 18:59:39 +0700

    add data fake

--- src/components/config/ConfigTable.tsx
+++ src/components/config/ConfigTable.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Table} from "antd";
 
 interface Item {
     id: number;
     name: string;
     email: string;
     // Define other properties of your item
+}
+interface DataType {
+    key: React.Key;
+    name: string;
+    age: number;
+    address: string;
 }
 
 function ConfigTable() {
@@ -26,27 +33,52 @@ function ConfigTable() {
         fetchData();
     }, []);
 
+    const dataExample: DataType[] = [];
+    for (let i = 0; i < 46; i++) {
+        dataExample.push({
+            key: i,
+            name: `Edward King ${i}`,
+            age: 32,
+            address: `London, Park Lane no. ${i}`,
+        });
+    }
+    const dataSource = [
+        {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+        },
+    ];
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+
     return (
         <div>
-            <h2>Read Table</h2>
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.email}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+            <h2>Config Table</h2>
+            <Table dataSource={data} columns={columns}/>
         </div>
     );
 }
